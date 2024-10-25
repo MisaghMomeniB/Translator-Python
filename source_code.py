cd59889f94859cf8a29075d60256f548c5237931
MisaghMomeniB <jenabemisagh@gmail.com>
2024-10-25 13:48:45 +0330

    Create the main application window

--- source_code.py
+++ source_code.py
@@ -1,3 +1,9 @@
 from tkinter import *
 from tkinter import ttk, messagebox
 from googletrans import LANGUAGES, Translator
+
+# Create the main application window
+root = Tk()
+root.title("Translator")
+root.geometry("1080x400")  # Set window size
+root.configure(bg="white")  # Set background color
